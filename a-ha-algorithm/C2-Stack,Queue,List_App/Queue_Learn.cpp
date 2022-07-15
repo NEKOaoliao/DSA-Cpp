0b5dc730075ec380b2d0833b5d74f567f8749b86
Zacharyyyyyy_Z <65473904+NEKOaoliao@users.noreply.github.com>
2022-07-15 14:03:43 +0800

    Queue STL Learn

---
+++ a-ha-algorithm/C2-Stack,Queue,List_App/Queue_Learn.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    queue<int> myqueue;
+    queue<int> ans;
+    int n, temp;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &temp);
+        myqueue.push(temp);
+    }
+
+    while (myqueue.size())
+    {
+        ans.push(myqueue.front());
+        myqueue.pop();
+        myqueue.push(myqueue.front());
+        myqueue.pop();
+    }
+
+    while (ans.size())
+    {
+        printf(" %d", ans.front());
+        ans.pop();
+    }
+
+    return 0;
+}
